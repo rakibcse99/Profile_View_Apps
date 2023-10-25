96457a5b0f62a0aee24d08b9209ae55fc5aab727
rakibcse99 <rakib.cse99@gmail.com>
2023-10-25 22:19:18 +0600

    Build(project setup):  - Add Architectue Componentent  - Add Coroutines  - Retrofit 2  - Hilt  - View Binding  - Follow MVVM Pattern

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    kotlin("kapt")
+    id ("kotlin-android")
+    id ("kotlin-kapt")
+    id ("dagger.hilt.android.plugin")
 }
 
 android {
@@ -27,21 +31,48 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 }
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    //image downloader
+    implementation ("io.coil-kt:coil-compose:2.4.0")
+   // def hilt_version = ("2.44")
+    //noinspection GradleDependency
+    implementation ("com.google.dagger:hilt-android:2.44")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    kapt ("com.google.dagger:hilt-compiler:2.44")
+
+    //viewmodel
+    val lifecycle_version = "2.6.1"
+    //noinspection GradleDependency
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
+    //lifecycle and viewModel
+//    implementation "androidx.lifecycle:lifecycle-runtime-ktx:2.6.1"
+//    implementation "androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1"
+//    implementation 'androidx.lifecycle:lifecycle-livedata-ktx:2.6.1'
+    //coroutines
+    val coroutinesVersion = "1.6.0"
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+    //noinspection GradleDependency
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesVersion")
+
+    // paging
+
+    val retrofitVersion = "2.9.0"
+    implementation ("com.squareup.retrofit2:retrofit:$retrofitVersion")
+    implementation ("com.squareup.retrofit2:converter-gson:$retrofitVersion")
+    // define a BOM and its version
+    implementation(platform("com.squareup.okhttp3:okhttp-bom:4.9.3"))
+    debugImplementation ("com.github.chuckerteam.chucker:library:4.0.0")
+    implementation ("com.squareup.okhttp3:logging-interceptor")
+    // glide libary image load
+    implementation ("com.github.bumptech.glide:glide:4.15.1")
+
 }
